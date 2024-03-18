350f46f305b5fb812a9bfe97d07445ba9c434919
Einas1996 <EINAS.BAWABIJI@STUDENT.HU.NL>
2024-03-18 13:39:08 +0100

    dropdown deleted from the home page

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="flex flex-wrap">
     <div class="w-1/3 bg-gray-200 p-4 rounded-md shadow-md mb-4 mr-4">
-      <h2 class="text-xl font-semibold">Panel 1</h2>
-      <p>Panel content</p>
+      <h2 class="text-xl font-semibold">Project A</h2>
+      <p>content</p>
     </div>
     <div class="w-1/3 bg-gray-200 p-4 rounded-md shadow-md mb-4 mr-4">
-      <h2 class="text-xl font-semibold">Panel 2</h2>
-      <p>Panel content</p>
+      <h2 class="text-xl font-semibold">Project B</h2>
+      <p>content</p>
     </div>
     <div class="w-1/3 bg-gray-200 p-4 rounded-md shadow-md mb-4">
-      <h2 class="text-xl font-semibold">Panel 3</h2>
-      <p>Panel content</p>
+      <h2 class="text-xl font-semibold">Project C</h2>
+      <p>content</p>
     </div>
-    <!-- Voeg meer panelen toe zoals nodig -->
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="home">
-    <ProjectDropdown></ProjectDropdown>
     <LayoutComponent></LayoutComponent>
-    <!-- <BarChart></BarChart> -->
   </div>
 </template>
 <script>
 import LayoutComponent from "@/components/LayoutComponent.vue";
-import ProjectDropdown from "@/components/ProjectDropdown.vue";
-// import BarChart from "@/components/BarChart.vue";
 
 export default {
   name: "HomeView",
   components: {
-    ProjectDropdown,
     LayoutComponent
-    // BarChart
   }
 };
 </script>
